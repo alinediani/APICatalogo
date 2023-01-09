01468a59df8700ae30927cc407388b80fb42c7d9
Aline Diani <63258951+alinediani@users.noreply.github.com>
2023-01-08 22:11:59 -0300

    Criação de classe AppDBcontext

---
+++ Context/AppDBcontext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDBcontext : DbContext
+    {
+        public AppDBcontext(DbContextOptions<AppDBcontext> options) : base(options) 
+        { }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
